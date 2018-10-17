e7993b5335a017db5b12eaa0742f85ada3d246fa
Mikhail Mamaev <mikhail@mikhail.Mikhail>
2018-10-17 20:25:47 +0300

    improved title drawing, legend added

--- draw.C
+++ draw.C
@@ -52,7 +52,7 @@ void draw(Int_t pdg)
     {
        Int_t b = i%3;
        profile[i] = (TProfile*) f->Get( Form("y vs v1, mpd, centrality %i",b) );
-       profile[0]->SetTitle("mpd-tracks");
+       profile[0]->SetTitle("");
 //       profile[i]->GetXaxis()->SetNdivisions(504);
        profile[i]->SetMarkerStyle(20);
        profile[i]->SetMarkerColor(2);
@@ -81,35 +81,66 @@ void draw(Int_t pdg)
     }
 
     TCanvas* canv = new TCanvas("canv","Main canvas",1500,700);
-    TPad* pad1 = new TPad("pad1","Pad 1",0.   ,0.1 ,0.33,0.9); //left pad
-    TPad* pad2 = new TPad("pad2","Pad 2",0.33 ,0.1 ,0.66,0.9); //middle pad
-    TPad* pad3 = new TPad("pad3","Pad 3",0.66 ,0.1 ,1.  ,0.9); //right pad
+    TPad* pad1 = new TPad("pad1","Pad 1",0.   ,0. ,0.33,0.92); //left pad
+    TPad* pad2 = new TPad("pad2","Pad 2",0.33 ,0. ,0.66,0.92); //middle pad
+    TPad* pad3 = new TPad("pad3","Pad 3",0.66 ,0. ,1.  ,0.92); //right pad
     TLatex* ltext = new TLatex();
+    auto cent0 = new TLatex();
+    auto cent1 = new TLatex();
+    auto cent2 = new TLatex();
     canv->cd();
     pad1->Draw();
     pad2->Draw();
     pad3->Draw();
-    auto legend = new TLegend(0.40,0.05,0.60,0.1);
+    auto legend = new TLegend(0.2,0.8,0.5,0.89);
     legend -> AddEntry(profile[0],"mc-tracks");
     legend -> AddEntry(profile[3],"mpd-tracks");
 
     pad1->cd();
-    profile[0]->SetTitle("0<b<5 [fm]");
+    profile[0]->SetTitle("");
     profile[0]->Draw();
     profile[3]->Draw("Same");
     line->Draw("Same");
+    legend -> Draw();
+    cent0->DrawLatexNDC(0.42,0.86,"0 #leq b #leq 5 [fm]");
     pad2->cd();
-    profile[1]->SetTitle("5<b<9 [fm]");
+    profile[1]->SetTitle("");
     profile[1]->Draw();
     profile[4]->Draw("Same");
     line->Draw("Same");
+    legend -> Draw();
+    cent1->DrawLatexNDC(0.42,0.86,"6 #leq b #leq 9 [fm]");
     pad3->cd();
-    profile[2]->SetTitle("9<b<16 [fm]");
+    profile[2]->SetTitle("");
     profile[2]->Draw();
     profile[5]->Draw("Same");
     line->Draw("Same");
-    canv->cd();
     legend -> Draw();
-    ltext->DrawLatex(0.4,0.93,"y vs v_{1} for protons");
+    cent2->DrawLatexNDC(0.4,0.86,"10 #leq b #leq 16 [fm]");
+    canv->cd();
+    switch (pdg)
+    {
+        case 2212:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for p^{+}");
+            break;
+        case -2212:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for p^{-}");
+            break;
+        case 211:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for #pi^{+}");
+            break;
+        case -211:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for #pi^{-}");
+            break;
+        case 321:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for K^{+}");
+            break;
+        case -321:
+            ltext->DrawLatex(0.4,0.93,"v_{1} vs y for protons");
+            break;
+        default:
+            ltext->DrawLatex(0.4,0.93,Form("v_{1} vs y for pdg=%i",pdg));
+    }
+    canv->SaveAs(Form("stat/v1_vs_y_%i.png",pdg));
 
 }
